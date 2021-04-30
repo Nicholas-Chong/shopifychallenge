c468846fc1f8d6f589eb192f72021a533e2c9e14
Nicholas-Chong <48600727+Nicholas-Chong@users.noreply.github.com>
2021-04-30 14:44:13 -0400

    Redo home to use my custom modal component

--- src/views/home.js
+++ src/views/home.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Card from '../components/card'
-import Skylight from 'react-skylight'
+import Modal from '../components/custom-modal'
 import '../css/home.css'
 import cogoToast from 'cogo-toast';
 
@@ -29,10 +29,8 @@ class Home extends React.Component {
 
   render() {
     if (this.state.showBanner === false) {
-      var message = "Please select 5 movies first!"
       if (this.banner) { this.banner.hide() }
     } else {
-      message = "Thank You!"
       this.banner = cogoToast.info("You have selected 5 movies!", {hideAfter: 0})
     }
 
@@ -43,23 +41,23 @@ class Home extends React.Component {
           {this.state.cards}
         <div class="submitButtonContainer">
           <button 
-            onClick={() => this.submitModal.current.show()}
+            onClick={() => {this.submitModal.current.openModal()}}
             class="submitButton">Submit</button>
         </div>
-        <Skylight
-          hideOnOverlayClicked 
-          ref={this.submitModal} 
-          title={message}>
+        <Modal id="submitModal" name="Thanks!" ref={this.submitModal}>
           <div class="about">
             <p>Currently, ths submit button does not actually do anything, but in a real application, this would be the point where a PUT request with the JSON stringified data would be sent to a server. The data would then be parsed and added to a database of some sort.</p>
-            <h2>About</h2>
-            <p>This simple web app is built with React. Almost all components were made from scratch, and make use of numerous React states and props.</p>
+            <p><strong>About</strong></p>
+            <p>This simple web app is built with React. All components were made from scratch, and make use of numerous React states and props.</p>
             <a href="https://github.com/Nicholas-Chong/shopifychallenge">Check out the source code on GitHub</a>
-            <h2>To Recruiters:</h2>
-            <p>I have a few other projects that showcase my skills as a developer. Please checkout my personal website to learn more about them! </p>
-            <a href="https://nicholas-chong.github.io/" target="_blank" rel="noreferrer">Personal website</a>
+            <p><strong>To Recruiters</strong></p>
+            <p>I have a few other projects that showcase my skills as a developer. My favourite project thus far is my COVID-19 dashboard, built with a headless Python Django backend and a React front end. The project has seen 7500+ unique users over it's lifetime! I've also built a corresponding Python Twitter bot that tweets out daily COVID-19 updates.</p>
+            <p><a href="https://nicholas-chong.github.io/" target="_blank" rel="noreferrer">Personal website</a></p>
+            <p><a href='https://twitter.com/OntarioCovid19' target="_blank" rel="noreferrer">@OntarioCovid19 Twitter Bot Account</a></p>
+            <p><a href="https://github.com/Nicholas-Chong/covid-data-api" target="_blank" rel="noreferrer">COVID-19 Dashboard Python Django backend and Twitter Bot</a></p>
+            <p><a href="https://github.com/Nicholas-Chong/covid-data-api" target="_blank" rel="noreferrer">COVID-19 Dashboard React frontend</a></p>
           </div>
-        </Skylight>
+        </Modal>
         <div class="footer">
           <a 
             href="https://github.com/Nicholas-Chong/shopifychallenge" 
